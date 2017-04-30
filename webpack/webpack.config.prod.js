63d7e6ea32eaeeb156ae433abcef9311c474b0e6
Antonin Riviere <antonin.rvr@gmail.com>
2017-04-30 19:25:24 +0200

    add build script :hammer:

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -9,32 +10,76 @@ module.exports = {
     app: './index.js',
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'app.bundle.js',
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/static/'
   },
   module: {
     rules: [
+      // linters
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
       {
         test: /\.js$/,
         use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015'] },
-        }],
-        exclude: [/node_modules/],
+          loader: 'babel-loader'
+        }]
       },
       {
         test: /\.styl$/,
-        use: ExtractTextPlugin.extract('css-loader!stylus-loader')
+        /*use: ExtractTextPlugin.extract({
+          use: 'css-loader!stylus-loader'
+        })*/
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'stylus-loader']
+        })
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: 'file-loader?name=image/[name].[ext]'
+      },
+      {
+        test: /node_modules/,
+        loader: 'ify-loader'
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        exclude: /node_modules/,
+        use: [
+          'raw-loader',
+          'glslify'
+        ]
       }
     ]
   },
+  resolve: {
+    alias: {
+      Config: path.resolve(__dirname, '../src/scripts/config/'),
+      Core: path.resolve(__dirname, '../src/scripts/core/'),
+      Utils: path.resolve(__dirname, '../src/scripts/utils/')
+    }
+  },
   plugins: [
     new ExtractTextPlugin({
-      filename: 'app.bundle.css'
+      filename: 'app.bundle.css',
+      allChunks: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static/images/'), 
+        to: path.resolve(__dirname, '../dist/images/') 
+      }
+    ]
+    ,{
+      debug: true 
     })
-  ],
-  /*devServer: {
-    contentBase: path.resolve(__dirname),
-  },*/
+  ]
 };
